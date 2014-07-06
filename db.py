5cb7b76138c5c61b962e45ee4154318605d74bd1
KeithYue <buaawangyue@gmail.com>
2014-07-06 16:39:01 +0800

    the unique index is the source file

--- db.py
+++ db.py
@@ -15,10 +15,10 @@ db = client['pingan']
 # news = db.news
 
 # create the db index
-db.blog.ensure_index('source_url', cache_for=300, unique=True, dropDups=True)
-db.news.ensure_index('source_url', cache_for=300, unique=True, dropDups=True)
-db.magazines.ensure_index('source_url', cache_for=300, unique=True, dropDups=True)
-db.bbs.ensure_index('source_url', cache_for=300, unique=True, dropDups=True)
+db.blog.ensure_index('src_file', cache_for=300, unique=True, dropDups=True)
+db.news.ensure_index('src_file', cache_for=300, unique=True, dropDups=True)
+db.magazines.ensure_index('src_file', cache_for=300, unique=True, dropDups=True)
+db.bbs.ensure_index('src_file', cache_for=300, unique=True, dropDups=True)
 db.weibo.ensure_index('src_file', cache_for=300, unique=True, dropDups=True)
 
 

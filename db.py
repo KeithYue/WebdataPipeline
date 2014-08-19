11a533788623d4e56a3440493a73bebca830b0ce
KeithYue <buaawangyue@gmail.com>
2014-08-19 14:12:11 +0800

    remove the src index for bbs and weibo

--- db.py
+++ db.py
@@ -18,7 +18,7 @@ db = client['pingan']
 db.blog.ensure_index('src_file', cache_for=300, unique=True, dropDups=True)
 db.news.ensure_index('src_file', cache_for=300, unique=True, dropDups=True)
 db.magazines.ensure_index('src_file', cache_for=300, unique=True, dropDups=True)
-db.bbs.ensure_index('src_file', cache_for=300, unique=True, dropDups=True)
+# db.bbs.ensure_index('src_file', cache_for=300, unique=True, dropDups=True)
 
 
 def clear_db():

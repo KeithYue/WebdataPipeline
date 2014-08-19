a72ea1fdb84373d844d4a064ed94b9eba5d08ab5
KeithYue <buaawangyue@gmail.com>
2014-08-19 14:11:27 +0800

    add logging information

--- parse_weibo.py
+++ parse_weibo.py
@@ -1,6 +1,12 @@
 # coding=utf-8
 # parse data of weibo for external use
-from content_extractor import update_weibodata
+import logging
+from content_extractor import update_socialmedial
+
+def update_weibodata():
+    update_socialmedial('weibo_data', 'weibo')
+    return
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     update_weibodata()
